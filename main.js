64a9104dec07168f297d07e02bcc0c0cbf8b86d2
l-Les <92374425+l-Les@users.noreply.github.com>
2023-05-31 00:49:25 +0800

    Delete main.js

--- main.js
+++
@@ -1,9 +0,0 @@
-function logIn() {
-    let userName = document.getElementById("Username").value;
-    let passWord = document.getElementById("Password").value;
-
-    console.log(userName);
-    console.log(passWord);
-    document.getElementById("Username").value = '';
-    document.getElementById("Password").value = '';
-}
